704f6e9f494a4e6cac7220372f7a7274d0030d35
Farhad Agzamov <farhad@thisistommy.com>
2015-12-07 18:06:50 +0000

    setting up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -21,3 +21,43 @@ gulp.task('lint', function() {
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
+
+gulp.task('concatScripts', function() {
+   return gulp.src([
+      'js/vendor/jquery-1.10.1.min.js',
+      'js/main.js', ])
+    .pipe(maps.init())
+    .pipe(concat('all.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function() {
+  return gulp.src("js/all.js")
+    .pipe(uglify().on('error', gutil.log))
+    .pipe(uglify())
+    .pipe(rename('all.min.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('watchFiles', function() {
+  gulp.watch('js/main.js', ['minifyScripts']);
+});
+
+gulp.task('clean', function() {
+  del(['build']);
+});
+
+gulp.task("build", ['minifyScripts'], function() {
+  return gulp.src([
+    'index.html', 
+    'css/**', 
+    'js/all.min.js',], { base: './'})
+      .pipe(gulp.dest('build'));
+});
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task("default", ["clean"], function() {
+  gulp.start('build');
+});
